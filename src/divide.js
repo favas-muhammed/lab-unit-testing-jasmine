94631148dbf1aef54835628030783910872a9324
Favas Muhammed <favassakkeer13@gmail.com>
2024-09-06 02:20:33 +0200

    first commit

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,20 @@
-function divide () {
-       
+function Divide(numOne,numTwo){
+const Quotient =0;
+if(numOne/numTwo == Number){
+    Quotient=numOne%numTwo;
+return Quotient;
+}
+if (numOne == undefined || numTwo == undefined) { 
+    return undefined;
+}
+if (numOne == 0) { 
+    return '';
+}
+if (numOne == 0 && numTwo !== 0) { 
+    return 0;
+}
+if (numTwo == 0) { 
+    return undefined;
+}
+return numOne / numTwo;    
 }
--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,33 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
-describe("Iteration 2", () => {
-    describe("Function - divide", () => {
-        
+
+describe("Iteration 2 | Divide", () => {
+    describe("Function - Divide", () => {
+        it("should be defined", () => {
+            expect(Divide).toBeDefined(); 
+        });
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should take two arguments", () => {
+        expect(Divide.length).toBe(2);
+        });
+        it("should return the Quotient of the division two numbers", () => {
+            expect(Divide(27, 9)).toEqual(3);
+            expect(Divide(8, 4)).toEqual(2);
+            expect(Divide(100, 50)).toEqual(2);
+          });
+          it("should return undefined if any of the arguments is not provided", () => {
+            expect(Divide(1)).toEqual(undefined);
+            expect(Divide()).toEqual(undefined);
+            expect(Divide(undefined,2)).toEqual(undefined);
+            expect(Divide(2,undefined)).toEqual(undefined);
+          });
+    
+          it('should return undefined if denominator is 0', () => {
+              expect(Divide(3, null)).toBeUndefined();
+              expect(Divide(undefined, 0)).toBeUndefined();
+              expect(Divide(3, 0)).toBeUndefined();
+          });
 
     })    
 })
-
-*/
